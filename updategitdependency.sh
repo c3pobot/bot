ae2c0c9dfbfe49554814bff8613e172ed4a94fd9
scuba <scuba75@gmail.com>
2023-06-22 12:44:13 +0000

    update

---
+++ updategitdependency.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo updating botsocketclient
+npm i --package-lock-only github:/c3pobot/botsocketclient
+echo updating mongowrapper
+npm i --package-lock-only github:/c3pobot/mongowrapper
+echo updating quewrapper
+npm i --package-lock-only github:/c3pobot/quewrapper
