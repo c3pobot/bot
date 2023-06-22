8d79a1d4bdc4cf86196a902d21a98944d6a6d04f
scuba <scuba75@gmail.com>
2023-06-22 14:27:28 +0000

    update

--- buildtestimage.sh
+++ buildtestimage.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-testRegistry=$(cat .env.build)
-container="$testRegistry/$1"
+file1=$(head -n 1 .testregistry)
+tag=$1
+if [[ "$file1" ]]
+then
+  echo 'personal registry set'
+  registry="${file1%%[[:cntrl:]]}"
+else
+  registry="ghcr.io"
+  echo "using ghcr.io for test build"
+fi
+container="${registry}/${tag}:test"
 echo building $container
+docker build -t $container .
+docker push $container
