a02f8a28ee019039cafcbb897dd2e4c1ac67b441
scuba75 <scuba75@gmail.com>
2024-05-22 14:25:49 -0500

    update

--- src/processCmds/autoComplete.js
+++ src/processCmds/autoComplete.js
@@ -6,11 +6,11 @@ const getChoices = (obj={})=>{
   let returnArray = []
   let key = dataList?.nameKeys[obj.name]
   if(!key) key = dataList?.nameKeys[obj.name?.split('-')[0]]
-  if(key) returnArray = (dataList?.autoObj[key]?.filter(x=>x.name.toLowerCase().includes(obj.value.toLowerCase())) || [])
+  if(key) returnArray = (dataList?.autoObj[key]?.filter(x=>x?.name?.toLowerCase().includes(obj.value.toLowerCase())) || [])
   if(returnArray.length > 0 && returnArray.length < 26) return returnArray
 }
 module.exports = (interaction)=>{
   if(!interaction?.isAutocomplete()) return
-  let choices = getChoices(interaction.options.getFocused(true))
+  let choices = getChoices(interaction?.options?.getFocused(true))
   if(choices) interaction.respond(choices)
 }
